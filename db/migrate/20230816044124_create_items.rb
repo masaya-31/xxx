8c12c58704f8c1d144a8404e637468ee4edba67c
MasayaIto <94973345+masaya-31@users.noreply.github.com>
2023-08-16 05:01:07 +0000

    モデル：テーブル作成、リレーション作成

--- db/migrate/20230816044124_create_items.rb
+++ db/migrate/20230816044124_create_items.rb
@@ -1,6 +1,11 @@
 class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
+      t.integer :genre_id, null: false
+      t.string :name, null: false, unique: true
+      t.string :description, null: false
+      t.integer :price_without_tax, null: false
+      t.boolean :is_sales, null: false, default: true
 
       t.timestamps
     end
